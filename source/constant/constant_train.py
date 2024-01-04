d531f45bd826ac2b8441ddc76d8bc651bc4bd2d2
Sachin Bidwai <94353415+SachinBidwai@users.noreply.github.com>
2024-01-04 09:20:09 +0530

    MongoDB URL KEY Updated in Constant_train.py file

--- source/constant/constant_train.py
+++ source/constant/constant_train.py
@@ -9,7 +9,7 @@ FILE_NAME = 'training_data.csv'
 TRAIN_FILE_NAME = 'train.csv'
 TEST_FILE_NAME = 'test.csv'
 
-MONGODB_URL_KEY = "mongodb+srv://datawave05:mydataCustChurn@datawave.aignamw.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_URL_KEY = "mongodb+srv://dataencite:R@021015@sachinbidwai.cppzdnv.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'db-customer-churn'
 
 # Data ingestion constant
